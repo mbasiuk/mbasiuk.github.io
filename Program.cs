1ae94e37b3b2869ecfaef57bcd4701f43a2c95ae
mbasiuk <mykhailo.basiuk@gmail.com>
2024-04-30 08:55:39 -0500

    add the actions

--- Program.cs
+++ Program.cs
@@ -4,4 +4,12 @@ var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
+app.MapGet("/hr", () => {
+    return Results.File("hr.html", contentType: "text/html");
+});
+
+app.MapGet("/resume", () => {
+    return Results.File(@"cv/cv.html", contentType: "text/html");
+});
+
 app.Run();
