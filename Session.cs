16328a6163a20632df93991f80d714a89d9aa59d
Mykhailo Basiuk <mykhailo.basiuk@gmail.com>
2024-06-08 19:12:29 -0500

    fuck you git

--- Session.cs
+++ Session.cs
@@ -14,6 +14,12 @@ public class Session : LiteEntity
     protected string? Ip { get; set; }
     protected int? Port { get; set; }
     protected string? UserAgent { get; set; }
+    protected string? Origin { get; set; }
+    protected string? Referer { get; set; }
+    protected string? Platform { get; set; }
+    protected string? UA { get; set; }
+    protected string? Mobile { get; set; }
+
 
     public Session(HttpContext context)
     {
@@ -26,7 +32,7 @@ public class Session : LiteEntity
             Connection.Open();
             using var cmd = Connection.CreateCommand();
             cmd.CommandTimeout = CommandTimeout;
-            cmd.CommandText = "INSERT INTO Session (session_id, connection_id, accept_lang, local_ip, local_port, ip, port, user_agent) VALUES(@session_id, @connection_id, @accept_lang, @local_ip, @local_port, @ip, @port, @user_agent)";
+            cmd.CommandText = "INSERT INTO Session (session_id, connection_id, accept_lang, local_ip, local_port, ip, port, user_agent, origin, referer, platform, ua, mobile) VALUES(@session_id, @connection_id, @accept_lang, @local_ip, @local_port, @ip, @port, @user_agent, @origin, @referer, @platform, @ua, @mobile)";
             cmd.Parameters.Add(new SqliteParameter("session_id", SessionId));
             CreateContextParams(cmd);
             var result = cmd.ExecuteNonQuery();
@@ -37,7 +43,7 @@ public class Session : LiteEntity
             using var reader = select.ExecuteReader();
             reader.Read();
             Expires = DateTimeOffset.FromUnixTimeSeconds(reader.GetInt32(0));
-            context.Response.Cookies.Append(Key, SessionId, new CookieOptions(){ Expires = Expires, Secure = true, HttpOnly = true, IsEssential = true });
+            context.Response.Cookies.Append(Key, SessionId, new CookieOptions() { Expires = Expires, Secure = true, HttpOnly = true, IsEssential = true });
             if (result == 0) { throw new Exception("unable to save"); }
         }
         context.Items[nameof(SessionId)] = SessionId;
@@ -57,6 +63,12 @@ public class Session : LiteEntity
         cmd.Parameters.Add(new SqliteParameter("ip", Ip));
         cmd.Parameters.Add(new SqliteParameter("port", Port));
         cmd.Parameters.Add(new SqliteParameter("user_agent", UserAgent));
+        
+        cmd.Parameters.Add(new SqliteParameter("origin", Origin));
+        cmd.Parameters.Add(new SqliteParameter("referer", Referer));
+        cmd.Parameters.Add(new SqliteParameter("platform", Platform));
+        cmd.Parameters.Add(new SqliteParameter("ua", UA));
+        cmd.Parameters.Add(new SqliteParameter("mobile", Mobile));
     }
 
     private void UpdateFromContext(HttpContext context)
@@ -68,11 +80,20 @@ public class Session : LiteEntity
         LocalIp ??= "-";
         LocalPort = context.Connection.LocalPort;
         LocalPort ??= 0;
-        Ip = context.Connection.RemoteIpAddress?.ToString();
-        Ip ??= "-";
+        Ip = context.Request.Headers["X-Forwarded-For"].ToString() ?? "-";
         Port = context.Connection.RemotePort;
         Port ??= 0;
         UserAgent = context.Request.Headers.UserAgent;
         UserAgent ??= "-";
+        Origin = context.Request.Headers.Origin;
+        Origin ??= "-";
+        Referer = context.Request.Headers.Referer;
+        Referer ??= "-";
+        Platform = context.Request.Headers["sec-ch-ua-platform"];
+        Platform ??= "-";
+        UA = context.Request.Headers["sec-ch-ua"];
+        UA ??= "-";
+        Mobile = context.Request.Headers["sec-ch-ua-mobile"];
+        Mobile ??= "-";
     }
 }
